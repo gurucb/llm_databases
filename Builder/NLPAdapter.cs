f90a74c0cf3c5ea135f369695deb0e65dbb7b90c
Guru Charan Bulusu <gurucb@microsoft.com>
2023-05-22 10:51:17 +0530

    Add project files.

---
+++ Builder/NLPAdapter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyntheticsGPTKQL
+{
+    internal class NLPAdapter
+    {
+        public static NLPProvider getNLPClient(String persona)
+        {
+            NLPProvider nlpClient;
+            if (persona.Equals("user"))
+            {
+                nlpClient = new UserNLPClient();
+            }
+            else
+            {
+                nlpClient = new StanfordNLPClient();
+            }
+            
+            return nlpClient;
+        }
+    }
+}
